c8c004c6a9168667a59c889cec8bfbe50726f9a6
Pedro Pina <pedropina@simei-gc.com>
2026-05-13 15:18:24 +0200

    IMPLEMENTADO EL GOAT SPIDER WOOO

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,31 +4,33 @@ vim.keymap.set("n", "<leader>e", function()
 end, { desc = "Explorer Snacks (Derecha)" })
 -- Jump to whatever is inside the quotes under the cursor
 vim.keymap.set("n", "gt", function()
-  -- 1. Get the current line
   local line = vim.api.nvim_get_current_line()
   local col = vim.api.nvim_win_get_cursor(0)[2] + 1
-
-  -- 2. Find the start and end of the quotes around the cursor
-  local s = line:find("['\"]", 1)
-  local start_quote, end_quote, path
-  
-  while s do
-    local e = line:find("['\"]", s + 1)
-    if e and s < col and col <= e then
-      path = line:sub(s + 1, e - 1)
+  local path = nil
+  for p1, q, c, p2 in line:gmatch("()([\"'])(.-)%2()") do
+    if col >= p1 and col <= p2 then
+      path = c
       break
     end
-    s = line:find("['\"]", e and e + 1 or nil)
   end
 
-  -- 3. If we found a path, tell Snacks to find that file
   if path and path ~= "" then
     Snacks.picker.files({
       pattern = path,
       title = "Jumping to: " .. path,
     })
   else
-    -- Fallback to default gf behavior if not in quotes
-    vim.cmd("normal! gf")
+    local ok, _ = pcall(vim.cmd, "normal! gf")
+    if not ok then
+      vim.notify("No se encontró una ruta válida", vim.log.levels.WARN)
+    end
   end
 end, { desc = "Go to File in Quotes" })
+vim.keymap.set("n", "<leader>mñ", function()
+  Snacks.picker.files({ ignored = true })
+end, { desc = "Buscar archivos (incluidos ocultos)" })
+
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
+vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>")
